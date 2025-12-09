2d4226bbaeac41f991c1aa92edf841d92c389677
miguel <madsmiguelangeld@gmail.com>
2025-12-09 12:17:51 -0400

    cliente controlador

--- backend/Controllers/ClienteController.ts
+++ backend/Controllers/ClienteController.ts
@@ -2,6 +2,7 @@ import { Request, Response } from "express";
 import { Cliente } from "../Models/Cliente";
 import { supabase } from "../src/Config/supabase";
 
+
 export class ClienteController {
   static async getCliente(req: Request, res: Response): Promise<void> {
     try {
