ad073e0a2d6332b1e524d5e5a49ea970b32afe67
miguel <madsmiguelangeld@gmail.com>
2025-12-09 12:16:54 -0400

    finales de los controlers

--- backend/Controllers/AutorController.ts
+++ backend/Controllers/AutorController.ts
@@ -3,6 +3,7 @@ import { supabase } from "../src/Config/supabase";
 import { AutorFunc } from "../Models/Autor";
 
 
+
 export class AutorController {
   static async getAutor(req: Request, res: Response): Promise<void> {
     try {
